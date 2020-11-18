519c595348ca03385c1dcd2847e63a5206dbbf57
melissaportfolio <ellieelise@live.com>
2020-11-18 14:04:49 -0700

    updated css

--- js/togglemenu.js
+++ js/togglemenu.js
@@ -1,3 +1,11 @@
+// function toggleMenu() {
+// 	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+// }
+// function toggleMenu() {
+//     document.querySelector('#navigation').classList.toggle('open');
+// }
+// document.querySelector('#menu').addEventListener('click', toggleMenu);
 function toggleMenu() {
-	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+    document.querySelector('#navigation').classList.toggle('open');
 }
+document.querySelector('#menu').addEventListener('click', toggleMenu);
